fd53a220cd589352779964c0b9139c2c133fb273
alvesmarcos <alves.henrique.marcos@gmail.com>
2019-08-30 13:01:27 -0300

    feat: add model state :construction:

---
+++ app/models/states.go
@@ -0,0 +1,17 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+// State - model that represents states of one task
+type State struct {
+	gorm.Model
+	ID          int    `gorm:"AUTO_INCREMENT" json:"id"`
+	Name        string `gorm:"unique;not_null" json:"name"`
+	Description string `json:"description"`
+	Slug        string `gorm:"unique;not_null" json:"slug"`
+}
+
+// NewState - to create a new state
+func NewState(name, description, slug string) *State {
+	return &State{Name: name, Description: description, Slug: slug}
+}

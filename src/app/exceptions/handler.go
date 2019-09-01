09a6c009a29485055b729a9526e09b2894d498ef
alvesmarcos <alves.henrique.marcos@gmail.com>
2019-09-01 01:14:00 -0300

    feat: add handler exceptions

--- src/app/exceptions/handler.go
+++ src/app/exceptions/handler.go
@@ -1,1 +1,23 @@
 package exceptions
+
+import (
+	"net/http"
+	"wikilibras-core/src/app/utils"
+)
+
+// ErrorShape -
+type ErrorShape struct {
+	Message interface{} `json:"message"`
+}
+
+// HandlerErrors - Check if exist errors
+func HandlerErrors(
+	err error, w http.ResponseWriter, message interface{}, code int,
+) bool {
+	if err != nil {
+		errors := ErrorShape{Message: message}
+		utils.SendJSON(w, errors, http.StatusNotFound)
+		return true
+	}
+	return false
+}
